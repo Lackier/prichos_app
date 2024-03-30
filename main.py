5552289907bad43d26ae599219aad7ddf5534a15
dchernov <d.chernov@globus-it.com>
2024-03-29 20:21:45 -0600

    application languages

--- main.py
+++ main.py
@@ -3,34 +3,168 @@ import tkinter
 from tkinter import ttk
 from tkcalendar import DateEntry
 import sqlite3
+from text_en import *
+import importlib
 
-ID = "ID"
-PRODUCT_NAME = "Product Name"
-PRICE = "Price"
-QUANTITY = "Quantity"
-WEIGHT = "Weight"
-DATE = "Date"
-PLACE = "Place"
-CATEGORIES = "Categories"
-COLUMNS = (ID, PRODUCT_NAME, PRICE, QUANTITY, WEIGHT, DATE, PLACE, CATEGORIES)
-
-PRODUCT_NAME_TEXT = "Product Name:"
-PRICE_TEXT = "Price:"
-QUANTITY_TEXT = "Quantity:"
-WEIGHT_TEXT = "Weight:"
-PURCHASE_DATE_TEXT = "Purchase Date:"
-PURCHASE_PLACE_TEXT = "Purchase Place:"
-CATEGORIES_TEXT = "Categories:"
-TO = "to"
-
-APPLY_BUTTON_TEXT = "Apply"
-SHOW_ALL_BUTTON_TEXT = "Show All"
-ADD_EDIT_BUTTON_TEXT = "Add/Edit"
-DELETE_BUTTON_TEXT = "Delete"
-SUBMIT_BUTTON_TEXT = "Submit"
-CLOSE_BUTTON_TEXT = "Close"
-
+COLUMNS = ()
 DATE_PATTERN="dd/mm/yyyy"
+window = 1
+
+def reload_window(language_code):
+    global window
+    if (window != 1):
+        window.destroy()
+    window = tkinter.Tk()
+    window.title("PrichosApp")
+    window.geometry("800x600")
+
+    language_module = load_language_module(language_code)
+
+    frame = tkinter.Frame(window)
+    frame.pack(fill="both", expand=True)
+
+    global tree
+    tree = ttk.Treeview(frame)
+
+    global COLUMNS
+    COLUMNS = (language_module.ID, language_module.PRODUCT_NAME, language_module.PRICE, language_module.QUANTITY, 
+           language_module.WEIGHT, language_module.DATE, language_module.PLACE, language_module.CATEGORIES)
+    tree["columns"] = COLUMNS
+    tree.heading(language_module.ID, text=language_module.ID)
+    tree.heading(language_module.PRODUCT_NAME, text=language_module.PRODUCT_NAME)
+    tree.heading(language_module.PRICE, text=language_module.PRICE)
+    tree.heading(language_module.QUANTITY, text=language_module.QUANTITY)
+    tree.heading(language_module.WEIGHT, text=language_module.WEIGHT)
+    tree.heading(language_module.DATE, text=language_module.DATE)
+    tree.heading(language_module.PLACE, text=language_module.PLACE)
+    tree.heading(language_module.CATEGORIES, text=language_module.CATEGORIES)
+
+    for column in tree["columns"]:
+        tree.column(column, width=100)
+    tree.column("#0", width=0)
+    tree.column(language_module.ID, width=20)
+    tree.column(language_module.PRICE, width=30)
+    tree.column(language_module.QUANTITY, width=30)
+    tree.column(language_module.WEIGHT, width=50)
+    tree.column(language_module.DATE, width=50)
+
+    for col in tree["columns"]:
+        tree.heading(col, text=col, command=lambda c=col: on_treeview_sort_column(tree, c, False))
+
+    vsb = tkinter.Scrollbar(frame, orient="vertical", command=tree.yview)
+    vsb.pack(side="right", fill="y")
+
+    tree.configure(yscrollcommand=vsb.set)
+    tree.pack(fill="both", expand=True)
+
+    tree.bind("<Double-1>", on_item_double_click)
+    tree.bind("<<TreeviewSelect>>", on_tree_select)
+
+
+    filters_frame = tkinter.Frame(window)
+    filters_frame.pack(pady=10)
+
+    first_row_filters_frame = tkinter.Frame(filters_frame)
+    first_row_filters_frame.pack(side="top", padx=5, pady=5)
+
+    product_name_filter_label = tkinter.Label(first_row_filters_frame, text=language_module.PRODUCT_NAME_TEXT)
+    product_name_filter_label.pack(side="left")
+    product_name_filter_entry = tkinter.Entry(first_row_filters_frame, width=15)
+    product_name_filter_entry.pack(side="left", padx=5)
+
+    price_filter_label = tkinter.Label(first_row_filters_frame, text=language_module.PRICE_TEXT)
+    price_filter_label.pack(side="left", padx=5)
+    price_filter_from_entry = tkinter.Entry(first_row_filters_frame, width=8)
+    price_filter_from_entry.pack(side="left", padx=(0, 5))
+    price_to_filter_label = tkinter.Label(first_row_filters_frame, text=language_module.TO)
+    price_to_filter_label.pack(side="left")
+    price_filter_to_entry = tkinter.Entry(first_row_filters_frame, width=8)
+    price_filter_to_entry.pack(side="left", padx=(0, 5))
+
+    quantity_filter_label = tkinter.Label(first_row_filters_frame, text=language_module.QUANTITY_TEXT)
+    quantity_filter_label.pack(side="left", padx=5)
+    quantity_filter_entry = tkinter.Entry(first_row_filters_frame, width=8)
+    quantity_filter_entry.pack(side="left", padx=(0, 5))
+
+    weight_filter_label = tkinter.Label(first_row_filters_frame, text=language_module.WEIGHT_TEXT)
+    weight_filter_label.pack(side="left", padx=5)
+    weight_filter_entry = tkinter.Entry(first_row_filters_frame, width=8)
+    weight_filter_entry.pack(side="left", padx=(5, 10))
+
+
+    second_row_filters_frame = tkinter.Frame(filters_frame)
+    second_row_filters_frame.pack(side="top", padx=5, pady=5)
+
+    purchase_date_filter_label = tkinter.Label(second_row_filters_frame, text=language_module.PURCHASE_DATE_TEXT)
+    purchase_date_filter_label.pack(side="left", padx=5)
+    purchase_date_filter_from_entry = DateEntry(second_row_filters_frame, date_pattern=DATE_PATTERN, width=10)
+    purchase_date_filter_from_entry.delete(0, "end")
+    purchase_date_filter_from_entry.pack(side="left", padx=0)
+    purchase_date_clear_button1 = tkinter.Button(second_row_filters_frame, text="<-", command=lambda: purchase_date_filter_from_entry.delete(0, "end"))
+    purchase_date_clear_button1.pack(side="left", padx=(5, 0))
+    purchase_date_to_filter_label = tkinter.Label(second_row_filters_frame, text=language_module.TO)
+    purchase_date_to_filter_label.pack(side="left")
+    purchase_date_filter_to_entry = DateEntry(second_row_filters_frame, date_pattern=DATE_PATTERN, width=10)
+    purchase_date_filter_to_entry.delete(0, "end")
+    purchase_date_filter_to_entry.pack(side="left", padx=0)
+    purchase_date_clear_button2 = tkinter.Button(second_row_filters_frame, text="<-", command=lambda: purchase_date_filter_to_entry.delete(0, "end"))
+    purchase_date_clear_button2.pack(side="left", padx=(5, 0))
+
+    purchase_place_filter_label = tkinter.Label(second_row_filters_frame, text=language_module.PURCHASE_PLACE_TEXT)
+    purchase_place_filter_label.pack(side="left", padx=5)
+    purchase_place_filter_entry = tkinter.Entry(second_row_filters_frame, width=15)
+    purchase_place_filter_entry.pack(side="left", padx=5)
+
+    categories_filter_label = tkinter.Label(second_row_filters_frame, text=language_module.CATEGORIES_TEXT)
+    categories_filter_label.pack(side="left", padx=5)
+    categories_filter_entry = tkinter.Entry(second_row_filters_frame, width=15)
+    categories_filter_entry.pack(side="left", padx=5)
+
+    filter_button = tkinter.Button(filters_frame, text=language_module.APPLY_BUTTON_TEXT, command=apply_filters)
+    filter_button.pack(pady=5)
+
+
+    button_frame = tkinter.Frame(window)
+    button_frame.pack(pady=10)
+
+    display_button = tkinter.Button(button_frame, text=language_module.SHOW_ALL_BUTTON_TEXT, command=lambda: display_data("id", "DESC"), width=20, pady=10)
+    display_button.grid(row=0, column=0, padx=5)
+
+    add_edit_button = tkinter.Button(button_frame, text=language_module.ADD_EDIT_BUTTON_TEXT, command=add_edit_item, width=20, pady=10)
+    add_edit_button.grid(row=0, column=1, padx=5)
+
+    delete_button = tkinter.Button(button_frame, text=language_module.DELETE_BUTTON_TEXT, command=delete_selected_item, state="disabled", width=20, pady=10)
+    delete_button.grid(row=0, column=2, padx=5)
+
+
+    languages_frame = tkinter.Frame(window)
+    languages_frame.pack(pady=10)
+
+    change_language_en_button = tkinter.Button(languages_frame, text="EN", command=lambda: change_language("en"))
+    change_language_en_button.grid(row=1, column=0, padx=5)
+    change_language_es_button = tkinter.Button(languages_frame, text="ES", command=lambda: change_language("es"))
+    change_language_es_button.grid(row=1, column=1, padx=5)
+    change_language_ru_button = tkinter.Button(languages_frame, text="RU", command=lambda: change_language("ru"))
+    change_language_ru_button.grid(row=1, column=2, padx=5)
+
+    create_table()
+    display_data()
+
+    window.mainloop()
+
+def load_language_module(language_code):
+    try:
+        module_name = f"text_{language_code}"
+        language_module = importlib.import_module(module_name)
+        return language_module
+    except ImportError:
+        print(f"Error: Language module {module_name} not found.")
+        return None
+    
+def change_language(language_code):
+    global language_module
+    language_module = load_language_module(language_code)
+    reload_window(language_code)
 
 def display_data(sort_column="id", sort_order="DESC", filters=None):
     for row in tree.get_children():
@@ -94,7 +228,7 @@ def create_table():
 
 def add_edit_item(selected_item=None):
     add_edit_window = tkinter.Toplevel(window)
-    add_edit_window.title(ADD_EDIT_BUTTON_TEXT)
+    add_edit_window.title(language_module.ADD_EDIT_BUTTON_TEXT)
     add_edit_window.transient(window)
     add_edit_window.grab_set()
 
@@ -116,13 +250,13 @@ def add_edit_item(selected_item=None):
         entry.grid(row=row, column=column+1, padx=5, pady=5)
         return entry
 
-    product_name_entry = create_label_entry_pair(add_edit_window, PRODUCT_NAME_TEXT, 0, 0)
-    price_entry = create_label_entry_pair(add_edit_window, PRICE_TEXT, 1, 0)
-    quantity_entry = create_label_entry_pair(add_edit_window, QUANTITY_TEXT, 2, 0)
-    weight_entry = create_label_entry_pair(add_edit_window, WEIGHT_TEXT, 3, 0)
-    purchase_date_entry = create_label_date_entry_pair(add_edit_window, PURCHASE_DATE_TEXT, 4, 0)
-    purchase_place_entry = create_label_entry_pair(add_edit_window, PURCHASE_PLACE_TEXT, 5, 0)
-    categories_entry = create_label_entry_pair(add_edit_window, CATEGORIES_TEXT, 6, 0)
+    product_name_entry = create_label_entry_pair(add_edit_window, language_module.PRODUCT_NAME_TEXT, 0, 0)
+    price_entry = create_label_entry_pair(add_edit_window, language_module.PRICE_TEXT, 1, 0)
+    quantity_entry = create_label_entry_pair(add_edit_window, language_module.QUANTITY_TEXT, 2, 0)
+    weight_entry = create_label_entry_pair(add_edit_window, language_module.WEIGHT_TEXT, 3, 0)
+    purchase_date_entry = create_label_date_entry_pair(add_edit_window, language_module.PURCHASE_DATE_TEXT, 4, 0)
+    purchase_place_entry = create_label_entry_pair(add_edit_window, language_module.PURCHASE_PLACE_TEXT, 5, 0)
+    categories_entry = create_label_entry_pair(add_edit_window, language_module.CATEGORIES_TEXT, 6, 0)
 
     if selected_item:
         conn = sqlite3.connect('data.db')
@@ -169,14 +303,14 @@ def add_edit_item(selected_item=None):
         add_edit_window.destroy()
         display_data()
 
-    submit_button = tkinter.Button(add_edit_window, text=SUBMIT_BUTTON_TEXT, command=submit)
+    submit_button = tkinter.Button(add_edit_window, text=language_module.SUBMIT_BUTTON_TEXT, command=submit)
     submit_button.grid(row=7, columnspan=2, padx=5, pady=5)
 
     def close_window():
         add_edit_window.grab_release()
         add_edit_window.destroy()
 
-    close_button = tkinter.Button(add_edit_window, text=CLOSE_BUTTON_TEXT, command=close_window)
+    close_button = tkinter.Button(add_edit_window, text=language_module.CLOSE_BUTTON_TEXT, command=close_window)
     close_button.grid(row=8, columnspan=2, padx=5, pady=5)
 
 def delete_selected_item():
@@ -252,123 +386,4 @@ def on_treeview_sort_column(treeview, col, reverse):
 
     treeview.heading(col, command=lambda: on_treeview_sort_column(treeview, col, not reverse))
 
-window = tkinter.Tk()
-window.title("PrichosApp")
-window.geometry("800x600")
-window.configure(bg="#FEFDED")
-
-frame = tkinter.Frame(window)
-frame.pack(fill="both", expand=True)
-
-tree = ttk.Treeview(frame)
-tree["columns"] = COLUMNS
-tree.heading(ID, text=ID)
-tree.heading(PRODUCT_NAME, text=PRODUCT_NAME)
-tree.heading(PRICE, text=PRICE)
-tree.heading(QUANTITY, text=QUANTITY)
-tree.heading(WEIGHT, text=WEIGHT)
-tree.heading(DATE, text=DATE)
-tree.heading(PLACE, text=PLACE)
-tree.heading(CATEGORIES, text=CATEGORIES)
-
-for column in tree["columns"]:
-    tree.column(column, width=100)
-tree.column("#0", width=0)
-tree.column(ID, width=20)
-tree.column(PRICE, width=30)
-tree.column(QUANTITY, width=30)
-tree.column(WEIGHT, width=50)
-tree.column(DATE, width=50)
-
-for col in tree["columns"]:
-    tree.heading(col, text=col, command=lambda c=col: on_treeview_sort_column(tree, c, False))
-
-vsb = tkinter.Scrollbar(frame, orient="vertical", command=tree.yview)
-vsb.pack(side="right", fill="y")
-
-tree.configure(yscrollcommand=vsb.set)
-tree.pack(fill="both", expand=True)
-
-tree.bind("<Double-1>", on_item_double_click)
-tree.bind("<<TreeviewSelect>>", on_tree_select)
-
-
-filters_frame = tkinter.Frame(window)
-filters_frame.pack(pady=10)
-
-first_row_filters_frame = tkinter.Frame(filters_frame)
-first_row_filters_frame.pack(side="top", padx=5, pady=5)
-
-product_name_filter_label = tkinter.Label(first_row_filters_frame, text=PRODUCT_NAME_TEXT)
-product_name_filter_label.pack(side="left")
-product_name_filter_entry = tkinter.Entry(first_row_filters_frame, width=15)
-product_name_filter_entry.pack(side="left", padx=5)
-
-price_filter_label = tkinter.Label(first_row_filters_frame, text=PRICE_TEXT)
-price_filter_label.pack(side="left", padx=5)
-price_filter_from_entry = tkinter.Entry(first_row_filters_frame, width=8)
-price_filter_from_entry.pack(side="left", padx=(0, 5))
-price_to_filter_label = tkinter.Label(first_row_filters_frame, text=TO)
-price_to_filter_label.pack(side="left")
-price_filter_to_entry = tkinter.Entry(first_row_filters_frame, width=8)
-price_filter_to_entry.pack(side="left", padx=(0, 5))
-
-quantity_filter_label = tkinter.Label(first_row_filters_frame, text=QUANTITY_TEXT)
-quantity_filter_label.pack(side="left", padx=5)
-quantity_filter_entry = tkinter.Entry(first_row_filters_frame, width=8)
-quantity_filter_entry.pack(side="left", padx=(0, 5))
-
-weight_filter_label = tkinter.Label(first_row_filters_frame, text=WEIGHT_TEXT)
-weight_filter_label.pack(side="left", padx=5)
-weight_filter_entry = tkinter.Entry(first_row_filters_frame, width=8)
-weight_filter_entry.pack(side="left", padx=(5, 10))
-
-second_row_filters_frame = tkinter.Frame(filters_frame)
-second_row_filters_frame.pack(side="top", padx=5, pady=5)
-
-purchase_date_filter_label = tkinter.Label(second_row_filters_frame, text=PURCHASE_DATE_TEXT)
-purchase_date_filter_label.pack(side="left", padx=5)
-purchase_date_filter_from_entry = DateEntry(second_row_filters_frame, date_pattern=DATE_PATTERN, width=10)
-purchase_date_filter_from_entry.delete(0, "end")
-purchase_date_filter_from_entry.pack(side="left", padx=0)
-purchase_date_clear_button1 = tkinter.Button(second_row_filters_frame, text="<-", command=lambda: purchase_date_filter_from_entry.delete(0, "end"))
-purchase_date_clear_button1.pack(side="left", padx=(5, 0))
-purchase_date_to_filter_label = tkinter.Label(second_row_filters_frame, text=TO)
-purchase_date_to_filter_label.pack(side="left")
-purchase_date_filter_to_entry = DateEntry(second_row_filters_frame, date_pattern=DATE_PATTERN, width=10)
-purchase_date_filter_to_entry.delete(0, "end")
-purchase_date_filter_to_entry.pack(side="left", padx=0)
-purchase_date_clear_button2 = tkinter.Button(second_row_filters_frame, text="<-", command=lambda: purchase_date_filter_to_entry.delete(0, "end"))
-purchase_date_clear_button2.pack(side="left", padx=(5, 0))
-
-purchase_place_filter_label = tkinter.Label(second_row_filters_frame, text=PURCHASE_PLACE_TEXT)
-purchase_place_filter_label.pack(side="left", padx=5)
-purchase_place_filter_entry = tkinter.Entry(second_row_filters_frame, width=15)
-purchase_place_filter_entry.pack(side="left", padx=5)
-
-categories_filter_label = tkinter.Label(second_row_filters_frame, text=CATEGORIES_TEXT)
-categories_filter_label.pack(side="left", padx=5)
-categories_filter_entry = tkinter.Entry(second_row_filters_frame, width=15)
-categories_filter_entry.pack(side="left", padx=5)
-
-filter_button = tkinter.Button(filters_frame, text=APPLY_BUTTON_TEXT, command=apply_filters)
-filter_button.pack(pady=5)
-
-
-button_frame = tkinter.Frame(window)
-button_frame.pack(pady=10)
-
-display_button = tkinter.Button(button_frame, text=SHOW_ALL_BUTTON_TEXT, command=lambda: display_data("id", "DESC"), width=20, bg="#A1C398", fg="white", pady=10)
-display_button.grid(row=0, column=0, padx=5)
-
-add_edit_button = tkinter.Button(button_frame, text=ADD_EDIT_BUTTON_TEXT, command=add_edit_item, width=20, bg="#A1C398", fg="white", pady=10)
-add_edit_button.grid(row=0, column=1, padx=5)
-
-delete_button = tkinter.Button(button_frame, text=DELETE_BUTTON_TEXT, command=delete_selected_item, state="disabled", width=20, bg="#A1C398", fg="white", pady=10)
-delete_button.grid(row=0, column=2, padx=5)
-
-
-create_table()
-display_data()
-
-window.mainloop()
+reload_window("en")
